f5e4aaca50cad44f6f7983e2d08c50dc3e23643f
Hristo Emilov Petkov CS2016 <hristo.petkov.2016@uni.strath.ac.uk>
2019-02-15 19:15:44 +0000

    Upload New File

---
+++ Prototype-3/src/controler/AbsorberKeyListener.java
@@ -0,0 +1,36 @@
+package controler;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import model.Model;
+import view.GUI;
+
+public class AbsorberKeyListener implements KeyListener {
+	
+	private Model model;
+	private GUI gui;
+	
+	public AbsorberKeyListener(Model m, GUI g) {
+		model = m;
+		gui = g;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		if(key == KeyEvent.VK_F) {
+//			model.absorberShoot(model.getBall());
+			gui.getTimer().start();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+
+	}
+}
