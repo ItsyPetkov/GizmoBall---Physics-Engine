7cb06e9cd5c3a5b86aa74327d26ab5452cda5209
Ian Richardson CS2016 <ian.richardson.2016@uni.strath.ac.uk>
2019-02-18 22:57:53 +0000

    populating scene with gizmos

--- Prototype-3/src/main/Main.java
+++ Prototype-3/src/main/Main.java
@@ -7,12 +7,24 @@ public class Main {
 	
 	public static void main(String args[]) {
 		Model model = new Model();
-		Gizmo S1 = new SquareBumper(1,1);
-		Gizmo T1 = new TriangleBumper(3,1);
-		Gizmo C1 = new CircleBumper(5, 1);
+		Gizmo S1 = new SquareBumper(1,3);
+		Gizmo T1 = new TriangleBumper(3,3);
+		Gizmo C1 = new CircleBumper(5, 3);
+		Gizmo S2 = new SquareBumper(3,16);
+		Gizmo T2 = new TriangleBumper(7,16);
+		Gizmo C2 = new CircleBumper(11, 16);
+		Gizmo S3 = new SquareBumper(15,3);
+		Gizmo T3 = new TriangleBumper(15,7);
+		Gizmo C3 = new CircleBumper(14, 11);
 		model.addGizmo(S1);
 		model.addGizmo(T1);
 		model.addGizmo(C1);
+		model.addGizmo(S2);
+		model.addGizmo(T2);
+		model.addGizmo(C2);
+		model.addGizmo(S3);
+		model.addGizmo(T3);
+		model.addGizmo(C3);
 		GUI gui = new GUI(model);
 	}
 }
