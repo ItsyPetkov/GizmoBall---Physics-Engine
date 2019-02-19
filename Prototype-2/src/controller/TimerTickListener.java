16c147a3cad86b4114049337e50c58568651361d
Hristo Emilov Petkov CS2016 <hristo.petkov.2016@uni.strath.ac.uk>
2019-02-19 12:22:10 +0000

    Update TimerTickListener.java

--- Prototype-2/src/controller/TimerTickListener.java
+++ Prototype-2/src/controller/TimerTickListener.java
@@ -4,19 +4,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import model.Model;
+import view.GUI;
 
 public class TimerTickListener implements ActionListener {
 
 	private Model model;
+	private GUI gui;
 	
-	public TimerTickListener(Model m) {
+	public TimerTickListener(Model m, GUI g) {
 		model = m;
+		gui = g;
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		model.moveBall();
+		gui.getTextField().setText(gui.result(model.getBall().getVelo().y()/25));
 	}
 
 }
