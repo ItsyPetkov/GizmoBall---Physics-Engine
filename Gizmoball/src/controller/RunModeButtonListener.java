428719b2fcf7a95b4fae47b42e476bd0988ea7d6
Hristo Emilov Petkov CS2016 <hristo.petkov.2016@uni.strath.ac.uk>
2019-03-05 22:43:42 +0000

    Upload New File

---
+++ Gizmoball/src/controller/RunModeButtonListener.java
@@ -0,0 +1,24 @@
+package controler;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RunModeButtonListener implements ActionListener {
+
+	public RunModeButtonListener() {
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getActionCommand().equals("Tick")) {
+			System.out.println("Ticking...");
+		}else if(e.getActionCommand().equals("Pause")) {
+			System.out.println("Paused...");
+		}else if(e.getActionCommand().equals("Run")) {
+			System.out.println("Running...");
+		}
+	}
+
+}
