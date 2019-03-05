96e221c2f0313fbe67f6b04c1ec694b0eda59bf7
Hristo Emilov Petkov CS2016 <hristo.petkov.2016@uni.strath.ac.uk>
2019-03-05 22:39:37 +0000

    Upload New File

---
+++ Gizmoball/view/Main.java
@@ -0,0 +1,17 @@
+package view;
+import model.Model;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Model model = new Model();
+		
+		new MainMenu().mainMenuFrame();
+//		InGameMenuRunMode inGameMenu = new InGameMenuRunMode();
+//		InGameMenuBuildMode inGameMenu = new InGameMenuBuildMode();
+//		BuildMode bm = new BuildMode(model);
+//		RunMode rm = new RunMode(model);
+	}
+
+}
