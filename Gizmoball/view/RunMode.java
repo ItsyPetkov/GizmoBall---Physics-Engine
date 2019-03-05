2952a96bcb86e15eb8274f417697ba20f6a0a781
Hristo Emilov Petkov CS2016 <hristo.petkov.2016@uni.strath.ac.uk>
2019-03-05 22:39:28 +0000

    Upload New File

---
+++ Gizmoball/view/RunMode.java
@@ -0,0 +1,68 @@
+package view;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.Timer;
+import javax.swing.border.EmptyBorder;
+
+import controler.EscapeKeyListener;
+import controler.RunModeButtonListener;
+import controler.TimerTickListener;
+import model.Model;
+
+public class RunMode extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private Model model;
+	private Board board;
+	private Timer timer;
+	
+	public RunMode(Model model) {
+		this.model = model;
+		setTitle("Gizmoball - Run Mode");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setBounds(100, 100, 510, 590);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		this.setVisible(true);
+//		this.addKeyListener(new AbsorberKeyListener(model, this));
+//		this.setFocusable(true);
+		this.addKeyListener(new EscapeKeyListener(state()));
+		this.setFocusable(true);
+		timer = new Timer(50, new TimerTickListener(model));
+		
+		JLabel lblWelcomeToGizmoball = new JLabel("Welcome to Gizmoball - Run Mode");
+		lblWelcomeToGizmoball.setFont(new Font("Tahoma", Font.ITALIC, 25));
+		lblWelcomeToGizmoball.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(lblWelcomeToGizmoball, BorderLayout.NORTH);
+		
+		board = new Board(model);
+		contentPane.add(board, BorderLayout.CENTER);
+		
+		JPanel buttonPanel = new JPanel(new GridLayout(1,1));
+		JButton btnTick = new JButton("Tick");
+		btnTick.addActionListener(new RunModeButtonListener());
+		JButton btnRun = new JButton("Run");
+		btnRun.addActionListener(new RunModeButtonListener());
+		JButton btnPause = new JButton("Pause");
+		btnPause.addActionListener(new RunModeButtonListener());
+		
+		buttonPanel.add(btnTick);
+		buttonPanel.add(btnRun);
+		buttonPanel.add(btnPause);
+		contentPane.add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
+	public static String state() {
+		return "running";
+	}
+}
