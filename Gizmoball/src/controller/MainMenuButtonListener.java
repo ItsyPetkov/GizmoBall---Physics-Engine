059e71619fe9b88f72b009b91d7b3d2627f68eff
Hristo Emilov Petkov CS2016 <hristo.petkov.2016@uni.strath.ac.uk>
2019-03-06 22:46:42 +0000

    Update MainMenuButtonListener.java

--- Gizmoball/src/controller/MainMenuButtonListener.java
+++ Gizmoball/src/controller/MainMenuButtonListener.java
@@ -11,14 +11,12 @@ import view.RunMode;
 
 public class MainMenuButtonListener implements ActionListener {
 
-	private MainMenu mm;
 	private RunMode rm;
 	private BuildMode bm;
 	private Model model;
 	
 	public MainMenuButtonListener() {
 		model = new Model();
-		mm = new MainMenu();
 	}
 	
 	@Override
@@ -26,14 +24,14 @@ public class MainMenuButtonListener implements ActionListener {
 		// TODO Auto-generated method stub
 		if(arg0.getActionCommand().equals("New Game")) {
 			bm = new BuildMode(model);
-			//need to make the frame disappear here
+			MainMenu.getMainMenuFrame().dispose();
 		}else if(arg0.getActionCommand().equals("Load Game")) {
 			model.addBall(new Ball(10,10,20,10));
 			model.addBall(new Ball(8,9,-20,5));
 			rm = new RunMode(model);
-			//need to make the frame disappear here
+			MainMenu.getMainMenuFrame().dispose();
 		}else if(arg0.getActionCommand().equals("Exit Game")) {
-			mm.exitGameFrame();
+			MainMenu.exitGameFrame();
 		}
 	}
 
