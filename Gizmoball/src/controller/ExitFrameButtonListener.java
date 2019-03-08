5c9741477a20121398c6c92e2d43c3e348e2ecf4
Hristo Emilov Petkov CS2016 <hristo.petkov.2016@uni.strath.ac.uk>
2019-03-08 19:37:10 +0000

    Update ExitFrameButtonListener.java

--- Gizmoball/src/controller/ExitFrameButtonListener.java
+++ Gizmoball/src/controller/ExitFrameButtonListener.java
@@ -7,17 +7,21 @@ import view.MainMenu;
 
 public class ExitFrameButtonListener implements ActionListener {
 	
-	public ExitFrameButtonListener() {
-		
-	}
-	
-	@Override
+@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
 		if(arg0.getActionCommand().equals("Leave")) {
 			System.exit(0);
 		}else if(arg0.getActionCommand().equals("Stay")) {
 			MainMenu.getExitFrame().dispose();
+		}else if(arg0.getActionCommand().equals("Quit Game ")) {
+			System.exit(0);
+		}else if(arg0.getActionCommand().equals("Cancel ")) {
+			InGameMenuBuildMode.getInGameMenuExitFrame().dispose();
+		}else if(arg0.getActionCommand().equals("Quit Game")) {
+			System.exit(0);
+		}else if(arg0.getActionCommand().equals("Cancel")) {
+			InGameMenuRunMode.getInGameMenuExitFrame().dispose();
 		}
 	}
 
