ec7f3d092cd415bc3fd2e6291e593d1bee191fff
Hristo Emilov Petkov CS2016 <hristo.petkov.2016@uni.strath.ac.uk>
2019-03-10 04:18:07 +0000

    Update InGameMenuButtonListener.java

--- Gizmoball/src/controller/InGameMenuButtonListener.java
+++ Gizmoball/src/controller/InGameMenuButtonListener.java
@@ -1,27 +1,43 @@
 package controller;
-
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import view.BuildMode;
+import view.InGameMenuBuildMode;
+import view.InGameMenuRunMode;
+import view.RunMode;
+
 public class InGameMenuButtonListener implements ActionListener {
 
+
+	
 	public InGameMenuButtonListener() {
-		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		if(arg0.getActionCommand().equals("Resume Game")) {
-			System.out.println("Resuming game...");
+		if(arg0.getActionCommand().equals("Resume Game ")) {
+			InGameMenuBuildMode.getInGameMenuFrame().dispose();
+		}else if(arg0.getActionCommand().equals("Resume Game")) {
+			InGameMenuRunMode.getInGameMenuFrame().dispose();
 		}else if(arg0.getActionCommand().equals("Enter Run Mode")) {
-			System.out.println("Entering run mode...");
+			for(Frame f: BuildMode.getFrames()) {
+				f.dispose();
+			}
+			//Need to enter RunMode here
 		}else if(arg0.getActionCommand().equals("Restart Game")) {
 			System.out.println("Restarting game...");
 		}else if(arg0.getActionCommand().equals("Enter Build Mode")) {
-			System.out.println("Entering build mode...");
+			for(Frame f: RunMode.getFrames()) {
+				f.dispose();
+			}
+			//Need to enter BuildMode here
+		}else if(arg0.getActionCommand().equals("Exit Game ")) {
+			InGameMenuBuildMode.inGameMenuExitGameFrame();
 		}else if(arg0.getActionCommand().equals("Exit Game")) {
-			System.out.println("Exitting game...");
+			InGameMenuRunMode.inGameMenuExitGameFrame();
 		}
 	}
 
