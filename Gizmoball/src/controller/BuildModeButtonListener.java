4fe291a8fd9018b1e4651cc698e529ff750a4311
Khaled Alqallaf <khaled.alqallaf.2017@uni.strath.ac.uk>
2019-03-16 17:48:38 +0000

    Dynamic Move Gizmo

--- Gizmoball/src/controller/BuildModeButtonListener.java
+++ Gizmoball/src/controller/BuildModeButtonListener.java
@@ -8,71 +8,71 @@ import java.awt.event.*;
 
 public class BuildModeButtonListener implements ActionListener {
 
-	Model model;
-	Board board;
-	int LtoPx = 25;
-	Gizmo target;
-	boolean dragging = false;
+    Model model;
+    Board board;
+    int LtoPx = 25;
+    Gizmo target;
+    boolean dragging = false;
 
-	public BuildModeButtonListener(Model m, Board b) {
-		this.model = m;
-		this.board = b;
-	}
+    public BuildModeButtonListener(Model m, Board b) {
+        this.model = m;
+        this.board = b;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		MouseListener[] mss = board.getMouseListeners();
-		for(int i=0; i<mss.length; i++){
-			board.removeMouseListener(mss[i]);
-		}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        MouseListener[] mss = board.getMouseListeners();
+        for(int i=0; i<mss.length; i++){
+            board.removeMouseListener(mss[i]);
+        }
 
-		MouseListener ml = new BuildModeMouseListener(model, e.getActionCommand(),board.getLtoPx());
-		board.addMouseListener(ml);
-		board.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mousePressed(MouseEvent me) {
-				if (e.getActionCommand().equals("Move")) {
-					System.out.println("Moving");
-					target = model.gizmoSearch(me.getX() / LtoPx, me.getY() / LtoPx);
-					if (target != null) {
-						if (target.getPos().x() == me.getX() / LtoPx && target.getPos().y() == me.getY() / LtoPx) {
-							model.moveGizmo(target, me.getX() / LtoPx, me.getY() / LtoPx);
-						} else {
-							dragging = true;
-						}
-					}
-				}
-			}
-		});
-		board.addMouseMotionListener(new MouseMotionAdapter() {
-			@Override
-			public void mouseDragged(MouseEvent me) {
-				if (e.getActionCommand().equals("Move")) {
-					System.out.println("Moving");
-					if (target != null) {
-						if (!dragging) {
-							model.moveGizmo(target, me.getX() / LtoPx, me.getY() / LtoPx);
-						}
-					}
-				}
-			}
-		});
-		if(e.getActionCommand().equals("Move")) {
-			System.out.println("Click an object to move, then click a position to move to");
-		} else if(e.getActionCommand().equals("Rotate")) {
-			System.out.println("Click an object to rotate");
-		} else if(e.getActionCommand().equals("Delete")) {
-			System.out.println("Click an object to delete");
-		} else if(e.getActionCommand().equals("Connect")) {
-			System.out.println("Connecting...");
-		} else if(e.getActionCommand().equals("Disconnect")) {
-			System.out.println("Disconneecting...");
-		} else if(e.getActionCommand().equals("Key Connect")) {
-			System.out.println("Connecting Key...");
-		} else if(e.getActionCommand().equals("Key Disconnect")) {
-			System.out.println("Disconnecting Key...");
-		}
-	}
+        MouseListener ml = new BuildModeMouseListener(model, e.getActionCommand(),board.getLtoPx());
+        board.addMouseListener(ml);
+        board.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent me) {
+                if (e.getActionCommand().equals("Move")) {
+                    System.out.println("Moving");
+                    target = model.gizmoSearch(me.getX() / LtoPx, me.getY() / LtoPx);
+                    if (target != null) {
+                        if (target.getPos().x() == me.getX() / LtoPx && target.getPos().y() == me.getY() / LtoPx) {
+                            model.moveGizmo(target, me.getX() / LtoPx, me.getY() / LtoPx);
+                        } else {
+                            dragging = true;
+                        }
+                    }
+                }
+            }
+        });
+        board.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent me) {
+                if (e.getActionCommand().equals("Move")) {
+                    System.out.println("Moving");
+                    if (target != null) {
+                        if (!dragging) {
+                            model.moveGizmo(target, me.getX() / LtoPx, me.getY() / LtoPx);
+                        }
+                    }
+                }
+            }
+        });
+        if(e.getActionCommand().equals("Move")) {
+            System.out.println("Click an object to move, then click a position to move to");
+        } else if(e.getActionCommand().equals("Rotate")) {
+            System.out.println("Click an object to rotate");
+        } else if(e.getActionCommand().equals("Delete")) {
+            System.out.println("Click an object to delete");
+        } else if(e.getActionCommand().equals("Connect")) {
+            System.out.println("Connecting...");
+        } else if(e.getActionCommand().equals("Disconnect")) {
+            System.out.println("Disconneecting...");
+        } else if(e.getActionCommand().equals("Key Connect")) {
+            System.out.println("Connecting Key...");
+        } else if(e.getActionCommand().equals("Key Disconnect")) {
+            System.out.println("Disconnecting Key...");
+        }
+    }
 
 }
